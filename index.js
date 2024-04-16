6b232c9c92b0f0e4d4934d1dc1a1b8105d6de55f
Aabid <aabidhussainpas@gmail.com>
2024-04-16 17:39:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+
+// importing the dependencies
+const mongoose = require('mongoose')
+const express = require('express')
+const Product = require('./models/product.model.js')
+
+
+const app = express()
+
+// attempting to use express.json() middleware in Express 
+// application to parse JSON data from incoming requests
+app.use(express.json())
+
+
+
+// Connecting to mongodb atlas database
+mongoose.connect("mongodb+srv://aabidhussainpas:thor7860@cluster01.k97jqfq.mongodb.net/TankTic?retryWrites=true&w=majority&appName=Cluster01")
+.then(()=>{
+    console.log("connected")
+    // running the server
+    app.listen(3000,()=>{
+        console.log(89)
+    })
+})
+.catch(()=>{
+    console.log("failed")
+})
+
+
+app.get('/',(req,res)=>{
+    res.send("hello90 jkworld")
+})
+
+// getting data from database
+app.get('/api/products', async (req,res)=>{
+    try{
+    const products = await Product.find({})
+    res.status(200).json(products)
+    }
+    catch(e){
+    res.status(500).json({message:e.message})
+
+    }
+})
+
+// Adding data in the database
+// res is response given by the program
+// req is the input by the user
+app.post('/api/products',async (req,res)=>{
+   try{
+   const product =  await Product.create(req.body)
+   res.status(200).json(product)
+   }
+   catch(e){
+    res.status(500).json({message:e.message})
+   }
+})
---
+++ models/product.model.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const ProductSchema = mongoose.Schema(
+
+    {
+        name: {
+            type: String,
+            required: [true, "Plese enter the product"]
+        }
+        ,
+
+        quantity: {
+            type: String,
+            required: true,
+            default: 0
+        },
+        price:{
+            type:Number,
+            required:true,
+            default:0
+        },
+        image:{
+            type:String,
+            required:false
+        }
+
+    },
+    // Provide two extra field 
+    // when created and when updated
+    {
+        timestamp:true
+    }
+)
+
+const Product = mongoose.model("Product",ProductSchema)
+
+module.exports = Product
