d74e158735523d15a023e3b80cda603a6e2b8e31
Aabid <aabidhussainpas@gmail.com>
2024-04-16 18:44:34 +0530

    crud operation included

--- index.js
+++ index.js
@@ -14,44 +14,91 @@ app.use(express.json())
 
 
 // Connecting to mongodb atlas database
-mongoose.connect("put connection string "
-.then(()=>{
-    console.log("connected")
-    // running the server
-    app.listen(3000,()=>{
-        console.log(89)
+mongoose.connect("put your connection string")
+    .then(() => {
+        console.log("connected")
+        // running the server
+        app.listen(3000, () => {
+            console.log(89)
+        })
     })
-})
-.catch(()=>{
-    console.log("failed")
-})
+    .catch(() => {
+        console.log("failed")
+    })
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("hello90 jkworld")
 })
 
 // getting data from database
-app.get('/api/products', async (req,res)=>{
-    try{
-    const products = await Product.find({})
-    res.status(200).json(products)
+app.get('/api/products', async (req, res) => {
+    try {
+        const products = await Product.find({})
+        res.status(200).json(products)
     }
-    catch(e){
-    res.status(500).json({message:e.message})
+    catch (e) {
+        res.status(500).json({ message: e.message })
+    }
+})
 
+app.get('/api/product/:id', async (req, res) => {
+    try {
+        // getting the id in the url
+        const {id } = req.params
+        const products = await Product.findById(id)
+        res.status(200).json(products)
+    }
+    catch (e) {
+        res.status(500).json({ message: e.message })
     }
 })
 
 // Adding data in the database
 // res is response given by the program
 // req is the input by the user
-app.post('/api/products',async (req,res)=>{
-   try{
-   const product =  await Product.create(req.body)
-   res.status(200).json(product)
-   }
-   catch(e){
-    res.status(500).json({message:e.message})
-   }
+app.post('/api/products', async (req, res) => {
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product)
+    }
+    catch (e) {
+        res.status(500).json({ message: e.message })
+    }
+})
+
+
+// update API
+app.put('/api/product/:id',async (req,res)=>{
+    try{
+        const {id} = req.params
+        const product = await Product.findByIdAndUpdate(id, req.body)
+        if (!product){
+            res.status(500).json({message:"Product not found"})
+            return
+        }
+        const updatedProduct = await Product.findById(id)
+        res.status(200).json(updatedProduct)
+    }
+    catch(e){
+        res.status(500).json({message:e.message})
+    }
+})
+
+// delete
+app.delete('/api/product/delete/:id',async (req,res)=>{
+    try{
+        const {id} = req.params
+        console.log(90)
+        const product = await Product.findByIdAndDelete(id)
+        console.log(90)
+        if (!product){
+            res.status(500).json({message:"Product not found"})
+            return
+        }
+        res.status(200).json({message:"Product deleted"})
+    }
+    catch(e){
+        res.status(500).json({message:e.message})
+    }
 })
